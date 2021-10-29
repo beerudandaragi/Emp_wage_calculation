42d25a1fe0a82c12b91c7adfb6b90c5e7c7291a1
beerappa <beerappadandaragi@gmail.com>
2021-10-29 16:19:57 +0530

    WelCome To Emp Wage Computation

---
+++ Display_emp_wage/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Display_emp_wage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee  Wage Computation");
+            int IS_FULLTIME = 1;
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if (empcheck == IS_FULLTIME)
+            {
+                Console.WriteLine("employee is present ");
+            }
+            else
+            {
+                Console.WriteLine("employee is absent");
+            }
+            Console.ReadKey();
+        }
+    }
+}
