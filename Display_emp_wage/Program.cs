8f4836c0eb59b40e1203a0e76de6133ea9104298
beerappa <beerappadandaragi@gmail.com>
2021-10-29 17:10:30 +0530

    Calculate Wage For Month

--- Display_emp_wage/Program.cs
+++ Display_emp_wage/Program.cs
@@ -11,6 +11,7 @@ namespace Display_emp_wage
         public const int Is_Part_Time = 1;
         public const int IS_FULLTIME = 2;
         public const int EMP_RATE_PER_HOURS = 80;
+        public const int Num_Of_Working_days = 10;
 
 
 
@@ -20,23 +21,30 @@ namespace Display_emp_wage
 
             int emphrs = 0;
             int empwage = 0;
-            Random random = new Random();
-            int empcheck = random.Next(0, 3);
-            switch (empcheck)
+            int totalempwage = 0;
+            for (int day = 0; day < Num_Of_Working_days; day++)
             {
-                case Is_Part_Time:
-                    emphrs = 4;
-                    break;
-                case IS_FULLTIME:
-                    emphrs = 8;
-                    break;
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
+                switch (empcheck)
+                {
+                    case Is_Part_Time:
+                        emphrs = 4;
+                        break;
+                    case IS_FULLTIME:
+                        emphrs = 8;
+                        break;
 
-                default:
-                    emphrs = 0;
-                    break;
+                    default:
+                        emphrs = 0;
+                        break;
+                }
+
+                empwage = emphrs * EMP_RATE_PER_HOURS;
+                totalempwage += empwage;
+                Console.WriteLine("Emp Wage:" + empwage);
             }
-            empwage = emphrs * EMP_RATE_PER_HOURS;
-            Console.WriteLine("Emp Wage:" + empwage);
+            Console.WriteLine("Total Emp Wage:" + totalempwage);
             Console.ReadKey();
         }
     }
