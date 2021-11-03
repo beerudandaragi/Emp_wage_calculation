62f351f0db2d786b274857fbe30e90a8be126e47
beerappa <beerappadandaragi@gmail.com>
2021-11-03 07:39:27 +0530

    To save The Total Wage foe Each Company

--- Display_emp_wage/Program.cs
+++ Display_emp_wage/Program.cs
@@ -6,17 +6,29 @@ using System.Threading.Tasks;
 
 namespace Display_emp_wage
 {
-    class Program
+   public class EmpWageBuilderObject
     {
         public const int Is_Part_Time = 1;
         public const int IS_FULLTIME = 2;
-        ////public const int EMP_RATE_PER_HOURS = 80;
-        ////public const int Num_Of_Working_days = 10;
-        ////public const int Max_Hrs_In_Month = 30;
+        private string Company;
 
-        public static int ComputeEmpWage(string Comapny,int EMP_RATE_PER_HOURS, int Num_Of_Working_days, int Max_Hrs_In_Month)
+        private int EMP_RATE_PER_HOURS = 80;
+        private int Num_Of_Working_days = 10;
+        private int Max_Hrs_In_Month = 30;
+        private int totalempwage;
+
+        public  EmpWageBuilderObject(string Company, int EMP_RATE_PER_HOURS, int Num_Of_Working_days, int Max_Hrs_In_Month)
         {
-             int emphrs = 0, totalemphrs = 0, totalworkingdays = 0;
+            this.Company=Company;
+            this.EMP_RATE_PER_HOURS=EMP_RATE_PER_HOURS;
+            this.Num_Of_Working_days=Num_Of_Working_days;
+            this.Max_Hrs_In_Month=Max_Hrs_In_Month;
+    }
+        public void ComputeEmpWage()
+        {
+
+
+            int emphrs = 0, totalemphrs = 0, totalworkingdays = 0;
             while (totalemphrs <= Max_Hrs_In_Month && totalworkingdays < Num_Of_Working_days)
             {
                 totalworkingdays++;
@@ -37,20 +49,28 @@ namespace Display_emp_wage
                 }
                 totalemphrs += emphrs;
                 Console.WriteLine("Days:" + totalworkingdays + "Emphrs:" + emphrs);
-                }
-            int totalempwage = totalemphrs * EMP_RATE_PER_HOURS;
-            Console.WriteLine("Total Emp Wage:" + totalempwage);
-            return totalempwage;
-            
-            
+            }
+            int totalempwage = totalemphrs *this. EMP_RATE_PER_HOURS;
+            Console.WriteLine("Total Emp Wage For company:"+Company  + "Is " + totalempwage);
+            Console.ReadLine();
         }
-                        static void Main(string[] args)
-                       {
-            ComputeEmpWage("HCL", 80, 10, 30);
+        public string Tostring()
+        {
+            return "Total Emp Wage For company:" + this.Company + "Is " + this.totalempwage; 
+        }
+        static void Main(string[] args)
 
-            ComputeEmpWage("TATA", 100, 15, 40);
-                            Console.ReadLine();
+        {
+
+            EmpWageBuilderObject HCL = new EmpWageBuilderObject("HCL", 100, 12, 30);
+            EmpWageBuilderObject Tata = new EmpWageBuilderObject("Tata", 120, 20, 40);
+            HCL.ComputeEmpWage();
+            Console.WriteLine(HCL.ToString());
+            Tata.ComputeEmpWage();
+            Console.WriteLine(Tata.ToString());
+
         }
+        
 
   
     }
